48e0e10453870e610cbd830b331343dec2582f17
Chris Rericha <tristipher@gmail.com>
2022-12-31 22:18:59 -0500

    [18EUS] Render stock market

--- assets/app/view/game/stock_market.rb
+++ assets/app/view/game/stock_market.rb
@@ -28,6 +28,8 @@ module View
         olive: '#d1e189',
         purple: '#9a4eae',
         peach: '#ffe5b4',
+        lightblue: '#add8e6',
+        violet: '#c71585',
       }.freeze
 
       # All markets
