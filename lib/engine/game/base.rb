441c318b6eed267eb3633e5fba19ebf30f12be81
Chris Rericha <crericha@gmail.com>
2023-03-01 00:00:40 -0500

    Allow companies to close silently

--- lib/engine/game/base.rb
+++ lib/engine/game/base.rb
@@ -1831,6 +1831,8 @@ module Engine
             next if entity&.name != ability.corporation
 
             company.close!
+            next if ability.silent
+
             @log << "#{company.name} closes"
           end
         end
