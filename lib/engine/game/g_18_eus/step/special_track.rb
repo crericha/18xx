b415107f555abbb110c64128f56b78cbe246f129
Chris Rericha <crericha@gmail.com>
2023-03-05 01:28:01 -0500

    [18EUS] Emergency train buying

--- lib/engine/game/g_18_eus/step/special_track.rb
+++ lib/engine/game/g_18_eus/step/special_track.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 require_relative '../../../step/special_track'
-require_relative 'tracker'
+require_relative 'remove_subsidies'
 
 module Engine
   module Game
     module G18EUS
       module Step
         class SpecialTrack < Engine::Step::SpecialTrack
-          include Tracker
+          include RemoveSubsidies
         end
       end
     end
