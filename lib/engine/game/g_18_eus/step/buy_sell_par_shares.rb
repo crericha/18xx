021cfbe858d341b52829c48e18de25e457e705a6
Michael Alexander <outsidepasser@gmail.com>
2023-03-05 04:09:21 -0500

    [18EUS] Fixed problem with selling the "first" bank share

--- lib/engine/game/g_18_eus/step/buy_sell_par_shares.rb
+++ lib/engine/game/g_18_eus/step/buy_sell_par_shares.rb
@@ -88,6 +88,10 @@ module Engine
             log_pass(action.entity)
             pass!
           end
+
+          def can_dump?(_entity, bundle)
+            bundle.corporation == @game.bny ? true : super
+          end
         end
       end
     end
