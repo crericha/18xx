02c537f0537e0b7053efe2947035679638c46449
Michael Alexander <outsidepasser@gmail.com>
2023-03-12 17:10:56 -0400

    Changes based on code inspection

--- lib/engine/game/base.rb
+++ lib/engine/game/base.rb
@@ -209,10 +209,10 @@ module Engine
 
       TRAIN_CLASS = Train
       DEPOT_CLASS = Depot
+      PLAYER_CLASS = Player
 
       MINORS = [].freeze
 
-      PLAYER_CLASS = Player
       PHASES = [].freeze
 
       LOCATION_NAMES = {}.freeze
