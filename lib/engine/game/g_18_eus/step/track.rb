c874e3d0c8db0064f3af8d559cff159e047dca3e
Chris Rericha <crericha@gmail.com>
2023-03-14 23:14:07 -0400

    [18EUS] Fix rebase errors

--- lib/engine/game/g_18_eus/step/track.rb
+++ lib/engine/game/g_18_eus/step/track.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../../../step/track'
-require_relative 'tracker'
 require_relative 'skip_bny'
 require_relative 'remove_subsidies'
 require_relative 'skip_end_set'
@@ -12,8 +11,8 @@ module Engine
       module Step
         class Track < Engine::Step::Track
           include SkipBny
-          include RemoveSubsidies
           include SkipEndSet
+          include RemoveSubsidies
 
           def process_lay_tile(action)
             return super unless free_home_city_lay?(action.entity, action.hex)
