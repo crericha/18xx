55d59206c8e8f0844a672fa1239a2db72cfd9240
Chris Rericha <crericha@gmail.com>
2023-03-21 23:03:36 -0400

    [18EUS] Implement subsidies

--- lib/engine/game/g_18_eus/step/special_track.rb
+++ lib/engine/game/g_18_eus/step/special_track.rb
@@ -9,6 +9,21 @@ module Engine
       module Step
         class SpecialTrack < Engine::Step::SpecialTrack
           include RemoveSubsidies
+
+          def available_hex(entity, hex)
+            if @game.rural_junction_companies.include?(entity) &&
+               ((hex.tile.color != :white) || !hex.tile.labels.empty? || hex.tile.cities.any?(&:tokened?))
+              return false
+            end
+
+            super
+          end
+
+          def potential_tile_colors(entity, _hex)
+            colors = super
+            colors << :green if entity.id == 'S9'
+            colors
+          end
         end
       end
     end
