ddd45996a4afeb059ff528c812f0c0aa708245f1
Chris Rericha <crericha@gmail.com>
2023-03-26 15:24:50 -0400

    [18EUS] Integrate A0, C6, C7, and C8

--- lib/engine/game/g_18_eus/step/special_choose.rb
+++ lib/engine/game/g_18_eus/step/special_choose.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require_relative '../../../step/base'
+require_relative '../../../step/special_choose'
 
 module Engine
   module Game
     module G18EUS
       module Step
-        class SpecialChoose < Engine::Step::Base
+        class SpecialChoose < Engine::Step::SpecialChoose
           def actions(entity)
             return [] if entity.owner != current_entity
 
@@ -23,12 +23,12 @@ module Engine
             if (private = @game.late_bloomer_companies.find { |c| c.name == action.choice })
               @game.companies << private
               private.owner = entity.owner
-              entity.owner << private
+              entity.owner.companies << private
             else
-              @game.bank.spend(@game.LATE_BLOOMER_CASH, entity.owner)
+              @game.bank.spend(@game.class::LATE_BLOOMER_CASH, entity.owner)
             end
 
-            @log << "#{entity.name} swapped for #{action.choice}"
+            @log << "#{entity.owner.name} swaps #{entity.name} for #{action.choice}"
             entity.close!
           end
         end
