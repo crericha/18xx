b453d32086e745f5b818e76d0ff13921e91f6cb8
Chris Rericha <crericha@gmail.com>
2023-09-04 00:57:39 -0400

    [18EUS] Trigger Soft Rust private before next round when train export causes trains to rust

--- lib/engine/game/g_18_eus/round/operating.rb
+++ lib/engine/game/g_18_eus/round/operating.rb
@@ -7,6 +7,22 @@ module Engine
     module G18EUS
       module Round
         class Operating < Engine::Round::Operating
+          def setup
+            super
+            @train_export_triggered = false
+          end
+
+          def finished?
+            return false unless super
+
+            unless @train_export_triggered
+              @game.export_train!
+              @train_export_triggered = true
+            end
+
+            super
+          end
+
           def next_entity!
             entity = @entities[@entity_index]
             if (subsidy = entity.companies.find { |c| c == @game.increase_stock_price_subsidy })
